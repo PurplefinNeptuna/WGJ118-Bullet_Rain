7d46b87669e96fcfb74d22bc0a28eaced8660f1b
PurplefinNeptuna <ilhamaryasuta@gmail.com>
2019-11-05 11:53:15 +0700

    add popcorn enemy

--- Assets/Resources/Scripts/Enemies/Popcorn0.cs
+++ Assets/Resources/Scripts/Enemies/Popcorn0.cs
@@ -1,5 +1,21 @@
 using UnityEngine;
 
 public class Popcorn0: BaseEnemy{
-	
+	private Vector3 targetDir;
+
+	public override void AIAwake(){
+		speed = 10f;
+		entranceDone = true;
+	}
+
+	private void Start() {
+		targetDir = Brain.main.player.thisTransform.localPosition;
+		targetDir += new Vector3(0f, 2f, 0f);
+		targetDir -= thisTransform.localPosition;
+		targetDir.Normalize();		
+	}
+
+	public override void AIUpdate(float deltaTime){
+		thisTransform.localPosition += targetDir * deltaTime * speed;
+	}
 }
