1895e7efbf3db7deeadc0bf999bc3c14a114056a
Ilham Aryasuta <ilhamaryasuta@gmail.com>
2019-11-20 13:00:08 +0700

    Add more enemy types

---
+++ Assets/Resources/Scripts/Enemies/Norm0.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Norm0 : BaseEnemy {
+	private Vector3 targetDir = Vector3.down;
+
+	public override void AIAwake() {
+		speed = 3f;
+		entranceDone = true;
+		health = 5;
+		thisTransform.localRotation = Utility.TopDownRotationFromDirection(targetDir);
+	}
+
+	public override void AIUpdate(float deltaTime) {
+		thisTransform.localPosition += targetDir * deltaTime * speed;
+		if(thisTransform.localPosition.y < -7f) {
+			Destroy(gameObject);
+		}
+	}
+}
